a942eff311ba3d364169735e0c7104a0a84c0b4c
Chris Kaltwasser <chris@chrisk.com>
2022-07-05 15:22:33 -0400

    added server no-show with overlay

---
+++ server-no-show-ovly.py
@@ -0,0 +1,56 @@
+# Welcome to Pyshone
+
+# This code is for the server
+# Lets import the libraries
+import socket, cv2, pickle, struct
+
+# Socket Create
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('HOST IP:', host_ip)
+port = 9999
+socket_address = (host_ip, port)
+
+# Socket Bind
+server_socket.bind(socket_address)
+
+# Socket Listen
+server_socket.listen(5)
+print("LISTENING AT:", socket_address)
+
+# Socket Accept
+while True:
+        client_socket,addr = server_socket.accept()
+        print('GOT CONNECTION FROM:', addr)
+        if client_socket:
+            frame_number = 0
+            vid = cv2.VideoCapture(0)
+            # Find OpenCV version
+            (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+            if int(major_ver) < 3:
+                fps = vid.get(cv2.cv.CV_CAP_PROP_FPS)
+            else:
+                fps = vid.get(cv2.CAP_PROP_FPS)
+            while (vid.isOpened()):
+                img,frame = vid.read()
+                frame_number += 1
+                text = "Sec: %2.1f" % (frame_number/fps)
+                frame = cv2.putText(
+                  img = frame,
+                  text = text,
+                  org = (10, 100),
+                  fontFace = cv2.FONT_HERSHEY_DUPLEX,
+                  fontScale = 3.0,
+                  color = (125, 246, 55),
+                  thickness = 3
+                )
+
+                a = pickle.dumps(frame)
+                message = struct.pack("Q", len(a))+a
+                client_socket.sendall(message)
+#                cv2.imshow('TRANSMITTING VIDEO', frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    client_socket.close()
+
